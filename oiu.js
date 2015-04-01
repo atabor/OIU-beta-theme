87d0518994b32efa58fbf6bbd075d71d06c88177
Al <alfonzo10@gmail.com>
2015-04-01 09:50:36 -0700

    working on metadata panel

--- oiu.js
+++ oiu.js
@@ -1,24 +1,18 @@
 (function ($) {
     $(document).ready(function () {
 
-	// manage story and comment forms
- //       $('.bear-comment-form').hide();
+	/* manage story and comment forms */
         $('.bear-story-form').hide();
- //       $('.bear-comment-icon').click(function () {
- //       	$('.bear-comment-form').toggle(); 
- //       });
         $('.bear-story-icon').click(function () {
         	$('.bear-story-form').toggle(); 
         });
 
-	// manage subscriptions panel
- 
-//        $('.follow-pane').show ();
+	/* manage subscriptions panel */
+        // $('.follow-pane').show ();
         $('.follow-button').click(function () {
         	$('.follow-pane').toggle(); 
                 $('.follow-help-anon').toggle();
         });
-
         $('.follow-help').hide ();
         $('.follow-question-icon').click(function () {
         	$('.follow-help').toggle(); 
@@ -27,13 +21,17 @@
         	$('.follow-help').toggle(); 
         });    
 
+        /* manage newsletter signup */
         $('#mc_embed_signup').hide ();
         $('.newsletter-button-wrapper').click(function () {
         	$('#mc_embed_signup').toggle(); 
         });
-//        $('.newsletter-icon').click(function () {
-//        	$('#mc_embed_signup').toggle(); 
-//        });        
+
+        /* manage metadata panel */
+        $('.metadata-button').click(function () {
+        	$('.metadata-pane').toggle(); 
+        	$('.author-pane').toggle(); 
+        });
 
     });
 })(jQuery);

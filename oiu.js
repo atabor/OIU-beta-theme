287a4afe91e388a81e72703c824cc375455be295
Al <alfonzo10@gmail.com>
2015-04-04 12:55:00 -0700

    tone down left sidebar content'

--- oiu.js
+++ oiu.js
@@ -10,27 +10,35 @@
 	/* manage subscriptions panel */
         // $('.follow-pane').show ();
         $('.follow-button').click(function () {
-        	$('.follow-pane').toggle(); 
-                $('.follow-help-anon').toggle();
+            $('.follow-pane').toggle(); 
+            $('.follow-help-anon').toggle();
         });
         $('.follow-help').hide ();
         $('.follow-question-icon').click(function () {
-        	$('.follow-help').toggle(); 
+            $('.follow-help').toggle(); 
         });
         $('.close-follow-help-text').click(function () {
-        	$('.follow-help').toggle(); 
+            $('.follow-help').toggle(); 
         });    
 
         /* manage newsletter signup */
         $('#mc_embed_signup').hide ();
         $('.newsletter-button-wrapper').click(function () {
-        	$('#mc_embed_signup').toggle(); 
+            $('#mc_embed_signup').toggle(); 
         });
 
         /* manage metadata panel */
         $('.metadata-button').click(function () {
-        	$('.metadata-pane').toggle(); 
-        	$('.author-pane').toggle(); 
+            $('.metadata-pane').toggle(); 
+            $('.author-pane').toggle();
+            if ($('div.metadata-button-wrapper').hasClass("open")) {
+                $('div.metadata-button-wrapper').removeClass("open");
+            }
+            if ($('div.metadata-button-wrapper').hasClass("closed")) {
+                $('div.metadata-button-wrapper').removeClass("closed").addClass("open");
+            } else {
+                $('div.metadata-button-wrapper').addClass("closed");
+            }
         });
 
     });
